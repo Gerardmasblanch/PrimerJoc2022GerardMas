398d925df82b9bafa019ceecb33982d88f38c0ba
Gerardmasblanch <101257175+Gerardmasblanch@users.noreply.github.com>
2022-03-14 14:28:09 +0100

    moviment i collision amb teclas
    moure el personatje amb les tecles

--- Sprite.gd
+++ Sprite.gd
@@ -1,28 +1,33 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var velocitat_angular= 90
-export var velocitat= Vector2(200,300)
+var velocitat_angular = 90
+export var velocitat = Vector2(200,200)
+var direccio = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position  = Vector2(500,300)
-	
-	
-	
-
+	position  = Vector2(0,0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation_degrees+= velocitat_angular * delta
-	position+= velocitat*delta
+	direccio = Vector2.ZERO
 	
-	if position.x>=1024 or position.x <= 0:
-		velocitat.x *= -1
-	if position.y>=600 or position.y <= 0:
-		velocitat.y *= -1
+	if Input.is_action_pressed("mou dreta"):
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed("mou esquerra"):
+		direccio += Vector2.LEFT
+	if Input.is_action_pressed("mou a dalt"):
+		direccio += Vector2.UP
+	if Input.is_action_pressed("mou a baix"):
+		direccio += Vector2.DOWN
+	position += direccio.normalized() * velocitat * delta
+	
+	
+	
+
 		
 	
 	
