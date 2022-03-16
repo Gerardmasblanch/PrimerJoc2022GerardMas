83ef406037db1142fef50cf9c1dcca726d1951b5
Gerardmasblanch <101257175+Gerardmasblanch@users.noreply.github.com>
2022-03-16 13:23:36 +0100

    començament cosos i contatce amb altres arees
    contacte i canvi de color al tocar una àrea que es un collisionsahpe2d

--- Imatges/personatge.gd
+++ Imatges/personatge.gd
@@ -9,7 +9,7 @@ export var velocitat = Vector2(200,200)
 var direccio = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position  = Vector2(0,0)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,10 +24,17 @@ func _process(delta):
 	if Input.is_action_pressed("mou a baix"):
 		direccio += Vector2.DOWN
 	position += direccio.normalized() * velocitat * delta
-	
-	
-	
 
-		
-	
-	
+
+
+func _on_personatje_area_entered(area:Area2D):
+	area.modulate = Color(0,1,0)
+	if area.is_in_group("Inici"):
+		modulate = Color(0,1,0)
+	if area.is_in_group("Final"):
+		modulate = Color(1,0,0)
+		area.modulate = Color(1,0,0)
+
+func _on_personatje_area_exited(area:Area2D):
+	modulate = Color(1,1,1)
+	area.modulate = Color(1,1,1)
